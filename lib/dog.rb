6915e0bcd03cd8888a38c2ba6ba3ed9465c876ce
Sarah Harrs <sarahharrs@gmail.com>
2019-07-11 19:11:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name = (dog_name)
+    @name= dog_name 
+  end
+  def name 
+    @name
+  end
+  def breed = (dog_breed)
+    @breed = dog_breed
+  end
+  def breed 
+    @breed
+  end
+end
+fido= dog.new 
+ralph = dog.name
+golden = dog.breed 
